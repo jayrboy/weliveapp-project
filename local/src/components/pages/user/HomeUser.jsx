87390a7e2e51c19627ffb42d0e9c52187b914580
jayrboy <j.jayox_@hotmail.com>
2024-03-10 17:44:55 +0700

    Add Navbar Public

--- local/src/components/pages/user/HomeUser.jsx
+++ local/src/components/pages/user/HomeUser.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from '../../../layout/Navbar'
+
 
 export default function HomeUser() {
   return (
     <React.Fragment>
+      <Navbar />
       <div
         className="card mt-5 mx-auto p-4 rounded"
         style={{ width: '400px', background: '#fff' }}
